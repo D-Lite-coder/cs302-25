75d3bf1f585eca4d0ed5dfd275a2c31309857bf4
David Isaac Carpenter <dcarpe23@hydra12.eecs.utk.edu>
2025-01-29 08:56:43 -0500

    solution.cpp started

--- challenge02/solution.cpp
+++ challenge02/solution.cpp
@@ -16,5 +16,43 @@ using namespace std;
 // Main Execution
 
 int main(int argc, char *argv[]) {
-    return EXIT_SUCCESS;
+
+	int index = 1;
+	while (index < argc) {
+
+		int N = stoi(argv[index]);
+		index++;
+	
+	vector<int> nums(N);
+
+	for(int i = 0; i < N; i++, index++) {
+	
+		nums[i] = stoi(argv[index]);
+	}
+
+	sort(nums.begin(), nums.end());
+
+	int minDiff = INT_MAX;
+	int currDiff, numOne, numTwo;
+	
+	for(int i = 1; i < N; i++) {
+			
+		currDiff = nums[i] - nums[i-1];
+
+		if(currDiff < minDiff) {
+
+			numOne = nums[i-1];
+			numTwo = nums[i];
+
+			minDiff = currDiff;
+
+		}
+	
+	}
+
+	cout << numOne << " " << numTwo << endl;
+
+	}
+
+    return 1;
 }
