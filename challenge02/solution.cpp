512821750ca415af66751638247c9536ab52dd21
David Isaac Carpenter <dcarpe23@hydra12.eecs.utk.edu>
2025-01-29 11:21:58 -0500

    solution.cpp complete

--- challenge02/solution.cpp
+++ challenge02/solution.cpp
@@ -1,58 +1,84 @@
-// Challenge 02: Closest Numbers
-// Name:  xxxxx
-
-// Brief description:
-
-// This code solves yyyy based on the following idea/implementation...
+/* Program name: Closest Numbers
+   Student name: David Carpenter
+   Net ID: dcarpe23
+   Student ID: 000722316
+   Program description: will read and sort a given vector of N ints. it will then find the smallest difference between the values. if there are muliple pair with the min difference it will print all of them in accending order */
 
 #include <algorithm>
 #include <climits>
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
 // Main Execution
 
-int main(int argc, char *argv[]) {
-
-	int index = 1;
-	while (index < argc) {
-
-		int N = stoi(argv[index]);
-		index++;
+int main() {
 	
-	vector<int> nums(N);
+	vector<int> minDiffList;
+	string line;
 
-	for(int i = 0; i < N; i++, index++) {
-	
-		nums[i] = stoi(argv[index]);
-	}
+	while (getline(cin, line)) {
+
+		int N = stoi(line); //gets vector size
+
+		//gets number vector
+		getline(cin, line);
+		stringstream numsLine(line);
+		vector<int> nums(N);
+		for (int i = 0; i < N; i++)
+		{
+			numsLine >> nums[i];
+		}
+
 
 	sort(nums.begin(), nums.end());
 
 	int minDiff = INT_MAX;
-	int currDiff, numOne, numTwo;
+	int currDiff;
 	
 	for(int i = 1; i < N; i++) {
 			
 		currDiff = nums[i] - nums[i-1];
 
-		if(currDiff < minDiff) {
-
-			numOne = nums[i-1];
-			numTwo = nums[i];
+		//compares the difference of the current two numbers in the vector to the current min diff.
+		if(currDiff < minDiff) { //clears current vector and adds new min diff.
 
 			minDiff = currDiff;
+			minDiffList.clear();
+			
+			minDiffList.push_back(nums[i-1]);
+			minDiffList.push_back(nums[i]);
+
+		}
+		else if (currDiff == minDiff) { //adds to min diff vector
+
+			minDiffList.push_back(nums[i-1]);
+			minDiffList.push_back(nums[i]);
 
 		}
 	
 	}
 
-	cout << numOne << " " << numTwo << endl;
+	int vectorSize = minDiffList.size();
+
+	//print min diff vector
+	for(int i = 0; i < vectorSize; i++) {
+
+		if(i == vectorSize-1) {
+
+			cout << minDiffList[i] << endl;
+		}
+		else {
+
+			cout << minDiffList[i] << " ";
+		}
 
 	}
 
-    return 1;
+	}
+
+    return 0;
 }
