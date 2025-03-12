c1b5f13831bd63f765d5fd6f7fd1e428deea303a
David Isaac Carpenter <dcarpe23@hydra12.eecs.utk.edu>
2025-03-12 10:25:10 -0400

    complete

--- challenge06/solution.cpp
+++ challenge06/solution.cpp
@@ -1,7 +1,47 @@
-// Challenge 06: Repeated DNA sequences
+/* Program Name: prims algorithm
+ * Student Name: David Carpenter
+ * Net ID: dcarpe23
+ * Student ID: 000722316
+ * Program Description: breaks a DNA string into substrings and looks for repetitive sequences*/
 
-int main(int argc, char *argv[]) {
+#include <iostream>
+#include <unordered_map>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string dna;
+
+    while (cin >> dna) {
+
+
+		unordered_map<string, int> subStrings; //all substrings
+		set<string> repeats; //repeated substrings
+		int length = dna.size();
+
+		//break DNA into substrings and check for reapeats
+		for(int index = 0; index <= length - 9; index++)
+		{
+			string sub = dna.substr(index, 9); //makes substring of length 9
+
+			subStrings[sub]++;
+			if(subStrings[sub] == 2) repeats.insert(sub); //if the substring is repeated, add to repeats
+
+		}
+
+		//print results
+		for (string sub : repeats) {
+
+			cout << sub << "\n";
+		}
+
+		cout << "-1" << endl;
+
+    }
+
+
     return 0;
 }
-
-
